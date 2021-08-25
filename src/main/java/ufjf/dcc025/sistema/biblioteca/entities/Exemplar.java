49d863d711447bc1eb39d15716b406c90eaa269d
MarianaSiano07 <58239978+MarianaSiano07@users.noreply.github.com>
2021-08-24 22:46:25 -0300

    entidade Exemplar

---
+++ src/main/java/ufjf/dcc025/sistema/biblioteca/entities/Exemplar.java
@@ -0,0 +1,53 @@
+package ufjf.dcc025.sistema.biblioteca.entities;
+
+import java.util.*;
+import java.lang.*;
+
+public class Exemplar extends Livro {
+    private Integer id;
+    private boolean ehNaoCircula;
+    private boolean disponivel;
+    private Livro livro;
+
+    public Exemplar(String nome, String autor, String editora, int quantAcervo, int quantEmprestimo, String ano, String edicao, Livro livro) {
+        super(nome, autor, editora, quantAcervo, quantEmprestimo, ano, edicao);
+        this.livro = livro;
+    }
+
+        public Integer getId() {
+            return id;
+        }
+
+        public boolean getEhAcervo() {
+            if(ehNaoCircula == true && disponivel == true) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+
+        public void setEhAcervo(boolean ehAcervo) {
+            boolean acervo = false;
+            acervo = getEhAcervo();
+
+            ehAcervo = acervo;
+        }
+
+        public boolean getDisponivel() {
+            if(disponivel == true){
+                return disponivel;
+            }
+            else {
+                return !disponivel;
+            }
+        }
+
+        public void setDisponivel(boolean disponivel) {
+            this.disponivel = disponivel;
+        }
+
+        public Livro getLivro() {
+            return livro;
+        }
+}
