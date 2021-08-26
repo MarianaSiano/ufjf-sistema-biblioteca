935ecd6c91164dc2fa2adc19e5cc8491172190d8
MarianaSiano07 <58239978+MarianaSiano07@users.noreply.github.com>
2021-08-26 15:29:37 -0300

    nova entidade

--- src/main/java/ufjf/dcc025/sistema/biblioteca/entities/Exemplar.java
+++ src/main/java/ufjf/dcc025/sistema/biblioteca/entities/Exemplar.java
@@ -4,7 +4,8 @@ import java.util.*;
 import java.lang.*;
 
 public class Exemplar extends Livro {
-    private Integer id;
+
+    private Integer idExemplar;
     private boolean ehNaoCircula;
     private boolean disponivel;
     private Livro livro;
@@ -13,41 +14,41 @@ public class Exemplar extends Livro {
         super(nome, autor, editora, quantAcervo, quantEmprestimo, ano, edicao);
         this.livro = livro;
     }
+    
+    public Integer getIdExemplar() {
+        return idExemplar;
+    }
 
-        public Integer getId() {
-            return id;
+    public boolean getEhAcervo() {
+        if (ehNaoCircula == true && disponivel == true) {
+            return true;
+        } 
+        
+        else {
+            return false;
         }
+    }
 
-        public boolean getEhAcervo() {
-            if(ehNaoCircula == true && disponivel == true) {
-                return true;
-            }
-            else {
-                return false;
-            }
+    public void setEhAcervo(boolean ehAcervo) {
+        boolean acervo = false;
+        acervo = getEhAcervo();
+
+        ehAcervo = acervo;
+    }
+
+    public boolean getDisponivel() {
+        if (disponivel == true) {
+            return disponivel;
+        } else {
+            return !disponivel;
         }
+    }
 
-        public void setEhAcervo(boolean ehAcervo) {
-            boolean acervo = false;
-            acervo = getEhAcervo();
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
 
-            ehAcervo = acervo;
-        }
-
-        public boolean getDisponivel() {
-            if(disponivel == true){
-                return disponivel;
-            }
-            else {
-                return !disponivel;
-            }
-        }
-
-        public void setDisponivel(boolean disponivel) {
-            this.disponivel = disponivel;
-        }
-
-        public Livro getLivro() {
-            return livro;
-        }
+    public Livro getLivro() {
+        return livro;
+    }
 }
